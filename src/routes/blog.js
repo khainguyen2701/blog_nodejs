8281ad3a4e760723b33194109411f03987e9fbbd
khainguyen2701 <nguyenvankhai2701@gmail.com>
2021-10-09 15:38:59 +0700

    husky and lint-staged

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -2,6 +2,6 @@ const express = require('express');
 const routes = express.Router();
 const blogController = require('../app/controllers/BlogController');
 
-    routes.use('/', blogController.index);
+                routes.use('/', blogController.index);
 
 module.exports = routes;
